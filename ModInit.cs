c1834acfa96f95190066039e13c2f31c265d17ac
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2020-06-08 10:44:16 -0400

    Initial commit

---
+++ ModInit.cs
@@ -0,0 +1,36 @@
+﻿using Harmony;
+using System;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace MoneyCanBuyLove
+{
+    public static class ModInit
+    {
+        public static MCBLSettings Settings = new MCBLSettings();
+        public const string HarmonyPackage = "us.tbone.MoneyCanBuyLove";
+        public static void Init(string directory, string settingsJSON)
+        {
+            try
+            {
+                ModInit.Settings = JsonConvert.DeserializeObject<MCBLSettings>(settingsJSON);
+            }
+            catch (Exception)
+            {
+                ModInit.Settings = new MCBLSettings();
+            }
+            var harmony = HarmonyInstance.Create(HarmonyPackage);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+    public class MCBLSettings
+    {
+        public string RepItemDefTypeAndID = "Item.HeatSinkDef.Gear_HeatSink_Generic_Standard";
+        public string[] excludedFactions = { "ClanJadeFalcon", "ClanWolf", "ClanGhostBear" };
+        public int RepModifier = 15;
+        public int MaxRepLimit = 20;
+
+    }
+
+}
+
---
+++ MoneyCanBuyLove.cs
@@ -0,0 +1,37 @@
+﻿using Harmony;
+using BattleTech;
+using System.Linq;
+
+namespace MoneyCanBuyLove
+{
+    static class MoneyCanBuyLove
+    {
+        [HarmonyPatch(typeof(BattleTech.SimGameState), "OnDayPassed")]
+        public static class OnDayPassed_Patch
+        {
+            public static void Postfix(SimGameState __instance, int timeLapse)
+            {
+
+                int countRep = __instance.CompanyStats.GetValue<int>(ModInit.Settings.RepItemDefTypeAndID);
+                string Owner = __instance.CurSystem.Def.OwnerValue.FriendlyName;
+
+                bool exclude = ModInit.Settings.excludedFactions.Any((string XFact) => __instance.CurSystem.Def.OwnerValue.FriendlyName.Contains(XFact));
+                if (exclude == true)
+                {
+                    __instance.CompanyStats.Set<int>(ModInit.Settings.RepItemDefTypeAndID, 0);
+                    return;
+                }
+                int getRep = __instance.CompanyStats.GetValue<int>("Reputation." + Owner);
+                int modRep = (countRep * ModInit.Settings.RepModifier) + getRep;
+                if(modRep > ModInit.Settings.MaxRepLimit)
+                {
+                    __instance.CompanyStats.Set<int>("Reputation." + Owner, ModInit.Settings.MaxRepLimit);
+                    __instance.CompanyStats.Set<int>(ModInit.Settings.RepItemDefTypeAndID, 0);
+                }
+                else
+                __instance.CompanyStats.Set<int>("Reputation." + Owner, modRep);
+                __instance.CompanyStats.Set<int>(ModInit.Settings.RepItemDefTypeAndID, 0);
+            }
+        }
+    }
+}
